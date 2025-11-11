cd935b9e392f538fc384f619add449f49c664392
Demian Sims <demiansims@gmail.com>
2025-11-10 19:27:26 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,93 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { KEYBOARD } from "./utils/keyboard";
 
 const turdleWord: string = "sssss";
 
-interface Word {
-  letter: string;
-  status: "correct" | "misplaced" | "incorrect" | null;
-}
-
 function App() {
-  const [guess, setGuess] = useState<string>("sssss");
-  const [word, setWord] = useState<Word[]>([]);
-  const [words, setWords] = useState<Word[][]>([]);
+  const [words, setWords] = useState<string[][]>([]);
+  const [currentCol, setCurrentCol] = useState<number>(0);
+  const [currentRow, setCurrentRow] = useState<number>(0);
 
-  function submitWord() {
-    const letterStatus: Word[] = [];
-    for (const [index, letter] of guess.split("").entries()) {
-      const trackedLetter = new Set();
-      if (!turdleWord.includes(letter)) {
-        letterStatus.push({ letter, status: "incorrect" });
-        continue;
-      }
-      const newIndex = trackedLetter.has(letter) ? index + 1 : index;
-      if (
-        turdleWord.includes(letter) &&
-        turdleWord.indexOf(letter, newIndex) !== index
-      ) {
-        letterStatus.push({ letter, status: "misplaced" });
-        trackedLetter.add(letter);
-      } else {
-        letterStatus.push({ letter, status: "correct" });
-      }
+  function handleKeyPress(letter: string) {
+    if (letter === "⌫") return handleDelete();
+    if (currentCol > 4) return;
+    if (currentCol < 4 && letter === "ENTER") return;
+    if (currentCol === 4 && letter === "ENTER") return handleSubmit();
+    setWords((prev) => {
+      if (!prev[currentRow]) return prev;
+      const newGrid = prev.map((row) => [...row]);
+
+      newGrid[currentRow][currentCol] = letter;
+
+      return newGrid;
+    });
+
+    setCurrentCol((col) => col + 1);
+  }
+
+  function handleDelete() {
+    if (currentCol === 0) return;
+    setWords((prev) => {
+      const newGrid = prev.map((row) => [...row]);
+      newGrid[currentRow][currentCol - 1] = "";
+
+      return newGrid;
+    });
+    setCurrentCol((col) => col - 1);
+  }
+
+  function handleSubmit() {
+    const wordRow = words[currentRow];
+    for (const [index, char] of wordRow.entries()) {
     }
-    setWord(letterStatus);
+    setWords((prev) => {
+      const newGrid = prev.map((row) => [...row]);
+      newGrid[currentRow] = letterStatus;
+      return newGrid;
+    });
+  }
+
+  function evaluateGuess(guess: string, targetString: string) {
+    const evalutions = guess;
   }
 
   useEffect(() => {
-    submitWord();
+    setWords(
+      Array.from({ length: 6 }, () => Array.from({ length: 5 }, () => ""))
+    );
   }, []);
 
-  useEffect(() => {
-    console.log("in useEffect: ", word);
-    const currentWordsIndex = words.length;
-    const winningWords = word
-      .map((letter) => letter.status)
-      .every((status) => status === "correct");
-
-    if (winningWords) {
-      console.log(`You won in ${currentWordsIndex + 1} tries!`);
-    }
-  }, [word, words]);
-
-  console.log(word, words);
-
-  return <div />;
+  return (
+    <>
+      <div className="container-grid">
+        {words?.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row?.map((box, colIndex) => (
+              <div key={colIndex} className="box">
+                {box}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="keyboard">
+        {KEYBOARD.map((row, rowIndex) => (
+          <div key={rowIndex} className="keyboard-row">
+            {row.map((key) => (
+              <button
+                key={key}
+                className="key"
+                onClick={() => handleKeyPress(key)}
+              >
+                {key}
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
